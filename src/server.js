8f76a174675f876eee1eff201f86a928779368aa
Adi Narayana Vemuru <vemuruadi@gmail.com>
2018-11-06 17:14:25 +0530

    added graphql api end point

--- src/server.js
+++ src/server.js
@@ -1,10 +1,34 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const expressGraphQL = require('express-graphql')
 
-const StartDb = require('./config/StartDb')
+
+//Local configuration for port, database start
 const port = require('./config/config').PORT
+const StartDb = require('./config/StartDb')
+
+//Schema for GraphQL end point
+const GraphQLSchema = require('./graphql')
+
+//init the server from express
 const server = express()
+
+//set Cors, url/body parsing settings
+server.use( cors({ optionsSuccessStatus: 200 }) )
+server.use( bodyParser.json({ limit: '50mb' }) )
+server.use( bodyParser.urlencoded({ limit: '50mb', extended: true }) )
+
+//GraphQL end point
+server.use('/graphql', expressGraphQL( () => {
+    return {
+        graphiql: true,
+        schema: GraphQLSchema
+    }
+}))
+
 
 //default API route - REST
 server.use('/', (req, res) => {
